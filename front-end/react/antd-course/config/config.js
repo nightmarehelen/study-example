809a51644b7058b55b140ce926b98ce7495c4260
nightmarehelen <nightmarehelen@qq.com>
2019-03-05 17:46:40 +0800

    no message

---
+++ front-end/react/antd-course/config/config.js
@@ -0,0 +1,36 @@
+export default {
+    singular: true,
+    plugins: [
+        ['umi-plugin-react', {
+            // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+            antd: true,
+            dva: true,
+        }],
+    ],
+    routes: [{
+        path: '/',
+        component: '../layout',
+        routes: [
+            {
+                path: '/',
+                component: 'Helloworld',
+            },
+            {
+                path: '/helloworld',
+                component: 'Helloworld'
+            },
+            {
+                path: 'puzzlecards',
+                component: './puzzlecards'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    {path: '/dashboard/analysis', component: 'Dashboard/Analysis'},
+                    {path: '/dashboard/monitor', component: 'Dashboard/Monitor'},
+                    {path: '/dashboard/workplace', component: 'Dashboard/Workplace'}
+                ]
+            },
+        ]
+    }],
+}
---
+++ front-end/react/antd-course/page/.umi/router.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+
+
+let Router = require('dva/router').routerRedux.ConnectedRouter;
+
+let routes = [
+  {
+    "path": "/",
+    "component": require('../../layout').default,
+    "routes": [
+      {
+        "path": "/",
+        "component": require('../Helloworld').default,
+        "exact": true
+      },
+      {
+        "path": "/helloworld",
+        "component": require('../Helloworld').default,
+        "exact": true
+      },
+      {
+        "path": "/puzzlecards",
+        "component": require('../puzzlecards').default,
+        "exact": true
+      },
+      {
+        "path": "/dashboard",
+        "routes": [
+          {
+            "path": "/dashboard/analysis",
+            "component": require('../Dashboard/Analysis').default,
+            "exact": true
+          },
+          {
+            "path": "/dashboard/monitor",
+            "component": require('../Dashboard/Monitor').default,
+            "exact": true
+          },
+          {
+            "path": "/dashboard/workplace",
+            "component": require('../Dashboard/Workplace').default,
+            "exact": true
+          },
+          {
+            "component": () => React.createElement(require('H:/workbench/personal/study-example/front-end/react/antd-course/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'page', hasRoutesInConfig: true })
+          }
+        ]
+      },
+      {
+        "component": () => React.createElement(require('H:/workbench/personal/study-example/front-end/react/antd-course/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'page', hasRoutesInConfig: true })
+      }
+    ]
+  },
+  {
+    "component": () => React.createElement(require('H:/workbench/personal/study-example/front-end/react/antd-course/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'page', hasRoutesInConfig: true })
+  }
+];
+window.g_routes = routes;
+window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+// route change handler
+function routeChangeHandler(location, action) {
+  window.g_plugins.applyForEach('onRouteChange', {
+    initialValue: {
+      routes,
+      location,
+      action,
+    },
+  });
+}
+window.g_history.listen(routeChangeHandler);
+routeChangeHandler(window.g_history.location);
+
+export default function RouterWrapper() {
+  return (
+<Router history={window.g_history}>
+      { renderRoutes(routes, {}) }
+    </Router>
+  );
+}
